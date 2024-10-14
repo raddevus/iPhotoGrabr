1d1db6f2634b8cd779555ac035a78079758decd3
raddevus <1radeutsch1@gmail.com>
2024-10-14 15:59:22 -0400

    Added code so that even if image file in zip files have same names the program will still extract them to the final destination.  Also, added code to provide a list of which files are duplicates.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
 
 if (args.Length < 2){
     Console.WriteLine("1. Put all your zipped photos in a directory. ");
     Console.WriteLine("Need target path.  \nUsage: $ iPhotoGrabr <path+zipFile> <target-folder>");
+    return;
+}
+
+if (args[0] == "find-dups"){
+    FindDuplicateFiles(args[1]);
     return;
 }
 
@@ -29,9 +36,54 @@ foreach (string f in allZipFiles){
         {
             if (!entry.Name.Contains(".mov",StringComparison.InvariantCultureIgnoreCase)){
                 var destinationPath = Path.Combine(targetPath,entry.Name);
-                Console.WriteLine($"copying {entry.Name} to {destinationPath}");
+                if (File.Exists(destinationPath)){
+                    destinationPath = Path.Combine(targetPath,$"{Path.GetFileNameWithoutExtension(Path.GetTempFileName())}{Path.GetExtension(entry.Name)}");
+                }
+                Console.WriteLine($"Extracting {entry.Name} to {destinationPath}");
                 entry.ExtractToFile(destinationPath);
             }
         }
     }
+}
+
+void FindDuplicateFiles(String path){
+    var allFiles = Directory.GetFiles(path);
+    Console.WriteLine($"Examining {allFiles.Length} files for duplicates.");
+    // SHA256-hash, filename
+    Dictionary<string, string> hashedFiles = new();
+    List<(string,string)> dupFiles = new List<(string,string)>();
+    int fileCounter = 0;
+    foreach (string f in allFiles){
+        Byte [] allBytes = File.ReadAllBytes(f);
+        var sha256 = GenSha256(allBytes);
+
+        // Console.WriteLine($"{sha256} : {Path.GetFileName(f)}");
+        if (!hashedFiles.TryAdd(sha256, Path.GetFileName(f))){
+            // Could not add the file because it's hash is already found
+            // which indicates that this is a duplicate of another which
+            // is already in hashedFiles
+            string origFile = "";
+            // get original file that this is a duplicate of
+            hashedFiles.TryGetValue(sha256,out origFile);
+            // add the dup to the dup file list
+            dupFiles.Add(($"{Path.GetFileName(f)}",$"{origFile}"));
+        }        
+        Console.Write($"{++fileCounter} ");
+    }
+    Console.WriteLine($"{Environment.NewLine} Examined {fileCounter} files.");
+    if (dupFiles.Count == 0){
+        Console.WriteLine("There are no duplicate image files.");
+        return;
+    }
+    foreach ((string,string)d in dupFiles){
+        Console.WriteLine("Duplicate file list...");
+        Console.WriteLine($"{d.Item1} same as {d.Item2}");
+    }
+}
+
+string GenSha256(Byte[] allFileBytes) 
+{ 
+    var sha = SHA256.Create();
+    byte[] hash = sha.ComputeHash(allFileBytes); 
+    return String.Concat(Array.ConvertAll(hash, x => x.ToString("x2"))); 
 }
